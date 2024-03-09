3c023a4da2137d3c71ecc543775cca14dd5dea1b
PavelProjects <paveloalexeev52@gmail.com>
2024-03-09 12:18:28 +0300

    version upgrade

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
+// Auto-generated code
 #include <Arduino.h>
-#include <WiFi.h>
 #include <ArduinoOTA.h>
+#include <SmartThing.h>
 
 #include "LouverController.h"
-#include "SmartThing.h"
 
-// Pins
+#define BUTTON_PIN 14
+
 #define MOTOR_FIRST_PIN 27
 #define MOTOR_SECOND_PIN 26
 #define POT_PIN 32
@@ -25,143 +26,134 @@
 #define DELAY_SETTING "delay"
 #define ACCURACY_SETTING "accuracy"
 
-using namespace Configurable::Action;
-
 LouverController controller;
 
+void addStates();
+void addSensors();
+void addActions();
+void addConfigEntries();
 void setupRestHandlers();
 void processConfig();
-void addDeviceStates();
-void addActionsHandlers();
-void registerSensors();
-void addConfigEntries();
-void addCallbacks();
+void addHooks();
 
 void setup() {
-    addDeviceStates();
-    registerSensors();
-    
-    bool started = SmartThing.init("louver");
-    if (started) {
-        if (SmartThing.wifiConnected()) {
-            setupRestHandlers();
-        }
-        controller.init(MOTOR_FIRST_PIN, MOTOR_SECOND_PIN, POT_PIN, LIGHT_SENSOR_PIN);
-        controller.addLedIndicator(SmartThing.getLed());
-        LOGGER.info("main", "Controller created");
+  addStates();
+  addSensors();
+  addActions();
+  addConfigEntries();
+  addHooks();
 
-        addActionsHandlers();
-        addConfigEntries();
-        addCallbacks();
-
-        processConfig();
-        LOGGER.info("main", "Config proceed");
-
-        JsonObject state = STSettings.getState();
-        if (state.containsKey(AUTO_MODE_STATE) && state[AUTO_MODE_STATE].as<bool>()) {
-            controller.enableAutoMode();
-        }
-    } else {
-        LOGGER.info("main", "Failed to init smart thing");
+  if (SmartThing.init("louver")) {
+    if (SmartThing.wifiConnected()) {
+      setupRestHandlers();
     }
-    LOGGER.info("main", "Setup finished");
+    LOGGER.info("main", "SmartThing successfully initialized");
+    controller.init(MOTOR_FIRST_PIN, MOTOR_SECOND_PIN, POT_PIN, LIGHT_SENSOR_PIN);
+    controller.addLedIndicator(SmartThing.getLed());
+    LOGGER.info("main", "Controller created");
+    processConfig();
+    LOGGER.info("main", "Config proceed");
+  } else {
+    LOGGER.error("main", "Failed to init SmartThing!");
+  }
+
+  if (SmartThing.wifiConnected()) {
+    ArduinoOTA.begin();
+  }
+
+  LOGGER.info("main", "Setup finished");
+}
+void loop() {
+  if (SmartThing.wifiConnected()) {
+    ArduinoOTA.handle();
+  }
+  delay(500);
 }
 
-void loop() {
-    if (!digitalRead(WIPE_BUTTON_PIN)) {
+void addActions() {
+  SmartThing.addActionHandler(ENABLE_AUTO_MODE, "Enable automode", []() {
+    return ActionResult(controller.enableAutoMode());
+  });
+  SmartThing.addActionHandler(DISABLE_AUTO_MODE, "Disable automode", []() {
+    return ActionResult(controller.disableAutoMode());
+  });
+  SmartThing.addActionHandler(OPEN, "Open", []() {
+    return ActionResult(controller.open());
+  });
+  SmartThing.addActionHandler(CLOSE, "Close", []() {
+    return ActionResult(controller.close());
+  });
+  SmartThing.addActionHandler(MIDDLE, "Middle", []() {
+    return ActionResult(controller.middle());
+  });
+  SmartThing.addActionHandler(BRIGHT, "Bright", []() {
+    return ActionResult(controller.bright());
+  });
+}
+void addSensors() {
+  SmartThing.addDigitalSensor("button", BUTTON_PIN);
+  SmartThing.addAnalogSensor("light", LIGHT_SENSOR_PIN);
+}
+void addStates() {
+  SmartThing.addDeviceState("position", []() {
+    return controller.getPosition();
+  });
+  SmartThing.addDeviceState("automode", []() {
+    if (controller.isAutoModeEnabled()) {
+      return "true";
+    }
+    return "false";
+  });
+}
+void addConfigEntries() {
+  SmartThing.addConfigEntry(DELAY_SETTING, "Automode update delay", "number");
+  SmartThing.addConfigEntry(ACCURACY_SETTING, "Motor accuracy", "number");
+  SmartThing.addConfigEntry(CLOSE_SETTING, "Light close value", "number");
+  SmartThing.addConfigEntry(OPEN_SETTING, "Light open value", "number");
+  SmartThing.addConfigEntry(BRIGHT_SETTING, "Light bright value", "number");
+}
+
+void addHooks() {
+  Hook::LambdaHook<int16_t> * hook = new Hook::LambdaHook<int16_t>(
+    [](int16_t &value) {
+      if (value == 0) {
         if (controller.isAutoModeEnabled()) {
-            controller.disableAutoMode();
+          controller.disableAutoMode();
         } else {
-            controller.enableAutoMode();
+          controller.enableAutoMode();
         }
         LOGGER.statistics();
-    }
+      }
+    },
+    true
+  );
+  hook->setTriggerValue(1);
+  hook->setTriggerDisabled(false);
+
+  HooksManager.addHook(SmartThing.getSensor("button"), hook);
 }
 
 void setupRestHandlers() {
-    RestController* rest = SmartThing.getRestController();
-
-    rest->addConfigUpdatedHandler([]() {
-        processConfig();
-    });
+  RestController.addConfigUpdatedHandler([]() {
+    processConfig();
+  });
 }
-
 void processConfig() {
-    JsonObject config = STSettings.getConfig();
-    int lightClose = config[CLOSE_SETTING];
-    int lightOpen = config[OPEN_SETTING];
-    int lightBright = config[BRIGHT_SETTING];
-    if (lightClose != 0 && lightOpen != 0 && lightBright != 0) {
-        controller.setLightValues(lightClose, lightOpen, lightBright);
-    }
+  JsonObject config = STSettings.getConfig();
+  int lightClose = config[CLOSE_SETTING];
+  int lightOpen = config[OPEN_SETTING];
+  int lightBright = config[BRIGHT_SETTING];
+  if (lightClose != 0 && lightOpen != 0 && lightBright != 0) {
+    controller.setLightValues(lightClose, lightOpen, lightBright);
+  }
 
-    int delay = config[DELAY_SETTING];
-    if (delay > 0) {
-        controller.setMonitorTaskDelay(delay);
-    }
+  int delay = config[DELAY_SETTING];
+  if (delay > 0) {
+    controller.setMonitorTaskDelay(delay);
+  }
 
-    uint8_t accuracy = config[ACCURACY_SETTING];
-    if (accuracy > 0) {
-        controller.setMotorAccuracy(accuracy);
-    }
-}
-
-void addConfigEntries() {
-    SmartThing.addConfigEntry(DELAY_SETTING, "Automode update delay", "number");
-    SmartThing.addConfigEntry(ACCURACY_SETTING, "Motor accuracy", "number");
-    SmartThing.addConfigEntry(CLOSE_SETTING, "Light close value", "number");
-    SmartThing.addConfigEntry(OPEN_SETTING, "Light open value", "number");
-    SmartThing.addConfigEntry(BRIGHT_SETTING, "Light bright value", "number");
-}
-
-void addDeviceStates() {
-    SmartThing.addDeviceState("automode", [](){
-        if (controller.isAutoModeEnabled()) {
-            return "true";
-        }
-        return "false";
-    });
-    SmartThing.addDeviceState("position", []() {
-        return controller.getPosition();
-    });
-    SmartThing.addDeviceState("led", []() {
-        return SmartThing.getLed()->isOn() ? "on" : "off";
-    });
-}
-
-void registerSensors() {
-    SmartThing.registerAnalogSensor("light", LIGHT_SENSOR_PIN);
-    SmartThing.registerSensor("position", []() {return controller.getMotorPosition();});
-
-    SmartThing.registerDigitalSensor("wipe_button", WIPE_BUTTON_PIN);
-}
-
-void addCallbacks() {
-    SmartThing.getCallbacksManager()->addDeviceStateCallback("automode", [](String * value) {
-        LOGGER.info("main", "Automode callback called. New value %s", *value);
-        JsonObject state = STSettings.getState();
-        state[AUTO_MODE_STATE] = value->equals("true");
-        STSettings.save();
-    });
-}
-
-void addActionsHandlers() {
-    SmartThing.addActionHandler(ENABLE_AUTO_MODE, "Enable automode", [](){
-        return ActionResult(controller.enableAutoMode());
-    });
-    SmartThing.addActionHandler(DISABLE_AUTO_MODE, "Disable automode", [](){
-        return ActionResult(controller.disableAutoMode());
-    });
-    SmartThing.addActionHandler(OPEN, "Set louver open position", [](){
-        return ActionResult(controller.open());
-    });
-    SmartThing.addActionHandler(CLOSE, "Set louver close position", [](){
-        return ActionResult(controller.close());
-    });
-    SmartThing.addActionHandler(MIDDLE, "Set louver middle position", [](){
-        return ActionResult(controller.middle());
-    });
-    SmartThing.addActionHandler(BRIGHT, "Set louver bright position", [](){
-        return ActionResult(controller.bright());
-    });
+  uint8_t accuracy = config[ACCURACY_SETTING];
+  if (accuracy > 0) {
+    controller.setMotorAccuracy(accuracy);
+  }
 }
